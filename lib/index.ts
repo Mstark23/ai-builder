925a105f9edee52ad0ba80577cfdedfc38c70aa5
Mstark23 <jhordanedouard@gmail.com>
2026-01-21 17:18:11 -0500

    Create index.ts

---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+/**
+ * VERKTORLABS - Library Exports
+ * 
+ * Central export point for all library utilities.
+ */
+
+// Industry Intelligence
+export * from './industry-intelligence';
+
+// Industry Options (for UI components)
+export * from './industry-options';
